5ad5cbe3336e4692961745c1a28ad5216d9e3a16
sahil-bhatta <s_bhatta195082@fanshaweonline.ca>
2023-08-02 19:03:21 -0400

    Added controls from storyboard and verified changing view of celcius/fahrenheit button when pressed

--- SimpleWeatherApp/ViewController.swift
+++ SimpleWeatherApp/ViewController.swift
@@ -9,9 +9,57 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func onFahrenheitButtonClicked(_ sender: UIButton) {
+        if isCelciusSelected {
+            isCelciusSelected = false
+            getWeather()
+        }
+    }
+    
+    @IBAction func onCelciusButtonClicked(_ sender: UIButton) {
+        if !isCelciusSelected {
+           isCelciusSelected = true
+            getWeather()
+        }
+    }
+    
+    
+    @IBOutlet weak var tvCity: UILabel!
+    
+    @IBOutlet weak var tvWeatherStatus: UILabel!
+    
+    @IBOutlet weak var ivWeather: UIImageView!
+    
+    @IBOutlet weak var tvTemperatue: UILabel!
+    
+    @IBOutlet weak var btnFahrenheit: UIButton!
+    
+    @IBOutlet weak var btnCelcius: UIButton!
+    
+    private var isCelciusSelected = true
+    
+    private var enabledColor = UIColor.blue
+    private var disabledColor = UIColor.tintColor
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        initView()
+    }
+    
+    func initView(){
+        self.btnFahrenheit.tintColor = disabledColor
+        getWeather()
+    }
+    
+    func getWeather(){
+        setButtonView()
+    }
+    
+    func setButtonView(){
+        self.btnCelcius.tintColor = isCelciusSelected ? enabledColor: disabledColor
+        self.btnFahrenheit.tintColor = !isCelciusSelected ? enabledColor: disabledColor
+        
     }
 
 
